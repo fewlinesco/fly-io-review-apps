f474d2fc7a6adecbc70aa82aa45eaa7ab3d0be76
Vincent Billey <vincent.billey@gmail.com>
2022-09-22 11:08:31 +0200

    Strategy immediate

--- entrypoint.sh
+++ entrypoint.sh
@@ -62,7 +62,7 @@ if [ -n "$INPUT_POSTGRES" ]; then
 fi
 
 if [ "$INPUT_UPDATE" != "false" ]; then
-  flyctl deploy --app "$app" --image "$image" --region "$region" --strategy rolling
+  flyctl deploy --app "$app" --image "$image" --region "$region" --strategy immediate
 fi
 
 if [ -n "$INPUT_SECRETS" ]; then
